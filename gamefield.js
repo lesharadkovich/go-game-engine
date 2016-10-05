a03baf2c4ec7c38f0b411e54dea0e13e81b2119e
Alyaksei Radkovich <aradkovi49811@mail.ru>
2016-10-05 22:45:48 +0300

    class

--- gamefield.js
+++ gamefield.js
@@ -1,40 +1,42 @@
-var field = {
-    width: 500,
-    cellNumber: 5,
-    backgroud: "rgb(255,165,0)",
-    canvasOffset: 20,
-    boardWidth: function () {
-        return this.width - 2 * this.canvasOffset;
-    },
-    drawOn: function (canvas) {
-        // console.log(JSON.stringify(this, null, 2));
+function Field() {
+    this.width = 500,
+    this.cellNumber = 5,
+    this.backgroud = "rgb(255,165,0)",
+    this.canvasOffset = 20
+}
 
-        ctx = canvas.getContext('2d');
+Field.prototype.boardWidth = function() {
+    return this.width - 2 * this.canvasOffset;
+}
 
-        canvas.width = this.width;
-        canvas.height = canvas.width;
+Field.prototype.drawOn = function (canvas) {
+    ctx = canvas.getContext('2d');
 
-        var x = this.canvasOffset;
-        var y = this.canvasOffset;
+    canvas.width = this.width;
+    canvas.height = canvas.width;
 
-        // console.log(this.boardWidth())
+    var x = this.canvasOffset;
+    var y = this.canvasOffset;
 
-        ctx.strokeRect(x, y, this.boardWidth(), this.boardWidth());
-        ctx.strokeRect(x - this.canvasOffset, y - this.canvasOffset, canvas.width, canvas.height);
-        ctx.fillStyle = this.backgroud;
-        ctx.fillRect(x, y, this.boardWidth(), this.boardWidth());
+    ctx.strokeRect(x, y, this.boardWidth(), this.boardWidth());
+    ctx.strokeRect(x - this.canvasOffset, y - this.canvasOffset, canvas.width, canvas.height);
+    ctx.fillStyle = this.backgroud;
+    ctx.fillRect(x, y, this.boardWidth(), this.boardWidth());
 
-        var cellWidth = this.boardWidth() / this.cellNumber;
-        for (i = 0; i < this.cellNumber; i++) {
-            ctx.moveTo(x + cellWidth * i, y);
-            ctx.lineTo(x + cellWidth * i, y + this.boardWidth());
-            ctx.stroke();
+    var cellWidth = this.boardWidth() / this.cellNumber;
+    for (i = 0; i < this.cellNumber; i++) {
+        ctx.moveTo(x + cellWidth * i, y);
+        ctx.lineTo(x + cellWidth * i, y + this.boardWidth());
+        ctx.stroke();
 
-            ctx.moveTo(x, y + cellWidth * i);
-            ctx.lineTo(x + this.boardWidth(), y + cellWidth * i);
-            ctx.stroke();
-        }
+        ctx.moveTo(x, y + cellWidth * i);
+        ctx.lineTo(x + this.boardWidth(), y + cellWidth * i);
+        ctx.stroke();
     }
-};
+}
+
+var field = new Field();
+
+field.canvasOffset = 30;
 
 field.drawOn(document.getElementById("example"));
